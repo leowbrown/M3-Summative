8d1849a561a8ba724b557c44dc1e78ec99ede44c
Juan Camilo CARO RODRIGUEZ <270024041@bun2-206-07.acg.ac.nz>
2021-04-07 13:17:53 +1200

    nav-menu-dropdown

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("hello");
+ 
+$('.second-nav').hide();
+ 
+$( document ).ready(function() {
+   // console.log( "document loaded" );
+$('.menu').click(function(){
+   $('.second-nav').toggle();
+})
+});
