baeb6f9c6b8aa174936647c0ccf3a060cc3127a0
Juan Camilo CARO RODRIGUEZ <270024041@bun2-206-07.acg.ac.nz>
2021-04-09 14:06:56 +1200

    created models folder

---
+++ models/post.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    item_name:{
+        type:String,
+        require:true
+    },
+    size:{
+        type:String,
+        require:true
+    },
+    price:{
+        type:String,
+         require:true
+    },
+    description:{
+        type:String,
+        require:true
+    },
+    photo:{
+        type:String,
+        require:true
+    },
+    comment: {
+        type:Array,
+        require: true
+    }
+},{
+    timestamps: true
+});
+
+module.exports = mongoose.model('Post', postSchema);
